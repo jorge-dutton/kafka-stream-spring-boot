1b789734ac9cf1a647fbd7c6af9309afcfa487a6
jorge-dutton <jorge.dutton@gmail.com>
2021-09-25 08:37:03 +0200

    first commit

---
+++ product-service/src/test/java/com/jdutton/pocs/kafka/products/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdutton.pocs.kafka.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
